2ae1b0351f2e747855384908c4ddfb84ed47636c
HyperHaxStudios <91548433+HyperHaxStudios@users.noreply.github.com>
2026-03-20 19:33:20 -0400

    Update EditScreen.cpp

--- src/GUI/Screens/Edit/EditScreen.cpp
+++ src/GUI/Screens/Edit/EditScreen.cpp
@@ -12,12 +12,10 @@
 #include <QListWidgetItem>
 #include <QMessageBox>
 #include <QSplitter>
-#include <QToolBar>
 
-#include "LPP.h"
-#include "GUI/Toolbars/EditScreenMenuBar.h"
 #include "GUI/Widgets/FileListWidget.h"
-#include "LCE/save/SaveFile.h"
+#include "GUI/Widgets/FileListWidget.h"
+#include "Save/SaveFile.h"
 
 namespace LPP::GUI::Screens::Edit {
     EditScreen::EditScreen(IO::FileType type, QWidget *parent) {
@@ -28,21 +26,5 @@ namespace LPP::GUI::Screens::Edit {
 
         mMainLayout->setContentsMargins(0, 0, 0, 0);
         mMainLayout->setSpacing(0);
-    }
-
-    void EditScreen::initMenuBar() {
-        MenuBars::EditScreenMenuBar *toolbar = new MenuBars::EditScreenMenuBar(this);
-        LegacyPlusPlus::sInstance.mainWindow->setMenuBar(toolbar);
-    }
-
-    void EditScreen::hideEvent(QHideEvent *event) {
-        QWidget::hideEvent(event);
-
-        if (QMenuBar *bar = LegacyPlusPlus::sInstance.mainWindow->menuBar())
-            bar->hide();
-    }
-
-    bool EditScreen::canSaveInPlace() {
-        return false;
     }
 }
